ac34de633001733c9e35cfa20961d4b83c3bf9f4
tammikott <144793453+tammikott@users.noreply.github.com>
2025-12-18 13:21:24 +0200

    Update config_example.py
    blabla

--- config_example.py
+++ config_example.py
@@ -1,7 +1,1 @@
-# KOPEERI SEE config.py JA MUUDA ANDMED
-# ÄRA COMMIDI config.py FAILI!
 
-DEBUG = False
-SECRET_KEY = 'sinu-super-salajane-võti-siia'
-DEMO_FILE = 'demo_critical.txt'  # Ohutu demo fail
-# ÄRA MUUTA OHTLIKUKS ILMA NÕUSOLEKUTA!
